35abc6e58c061048cb5c64db217869f398492b8c
minsiuuuu <kimms010107@gmail.com>
2024-12-25 22:43:27 +0900

    필수과제 제출

---
+++ Cpp_Assignment_2/Cpp_Assignment_2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// 필수과제
+class Animal
+{
+public:
+    virtual void makeSound() = 0;
+    virtual ~Animal() = default;
+};
+
+class Dog : public Animal
+{
+public:
+    void makeSound() override
+    {
+        cout << "멍멍!" << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void makeSound() override
+    {
+        cout << "야옹~" << endl;
+    }
+};
+
+class Cow : public Animal
+{
+public:
+    void makeSound() override
+    {
+        cout << "음머~" << endl;
+    }
+};
+
+int main()
+{
+    // 필수과제
+    cout << "|=== 필수과제 ===|" << endl;
+    vector<unique_ptr<Animal>> arr;
+
+    arr.push_back(make_unique<Dog>());
+    arr.push_back(make_unique<Cat>());
+    arr.push_back(make_unique<Cow>());
+
+    for (const auto& obj : arr)
+    {
+        obj->makeSound();
+    }
+}
